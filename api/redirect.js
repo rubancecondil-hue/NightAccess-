99e5ec6cfe1eda970cfb5b2b0865b25033f83fa6
Rubance koundilya <rubancecondil@gmail.com>
2026-01-01 01:38:04 +0530

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,21 +1,17 @@
-import fs from "fs";
-import path from "path";
-
 export default function handler(req, res) {
-  try {
-    const slug = req.query.slug;
+  const slug = req.query.slug;
 
-    const filePath = path.join(process.cwd(), "data", "links.json");
-    const links = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  const links = {
+    google: "https://google.com",
+    yt: "https://youtube.com",
+    insta: "https://instagram.com"
+  };
 
-    if (!links[slug]) {
-      res.status(404).send("Link not found");
-      return;
-    }
-
-    res.writeHead(302, { Location: links[slug] });
-    res.end();
-  } catch (error) {
-    res.status(500).send("Server error");
+  if (!links[slug]) {
+    res.status(404).send("Link not found");
+    return;
   }
+
+  res.writeHead(302, { Location: links[slug] });
+  res.end();
 }
