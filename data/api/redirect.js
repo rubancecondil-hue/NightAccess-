75d340e9c5fc85222a80494c7541989c17e80d3c
Rubance koundilya <rubancecondil@gmail.com>
2026-01-01 01:10:44 +0530

    Delete data directory

--- data/api/redirect.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const slug = req.query.slug;
-
-  if (slug === "google") {
-    return res.redirect("https://google.com");
-  }
-
-  res.status(404).send("Link not found");
-}
